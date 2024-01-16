b1fa7fb60c05e90ba1ed85f18d4bd76e604629c1
Fahim Kamal Ahmed <fahimkamal63@gmail.com>
2024-01-16 17:44:53 +0600

    Not building

---
+++ Pancake_package/Assets/_Root/Scripts/AdsShower.cs
@@ -0,0 +1,68 @@
+using Pancake.Monetization;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class AdsShower : MonoBehaviour
+{
+    [FormerlySerializedAs("interstatialBtn")] [SerializeField] private Button interstitialBtn;
+    [SerializeField] private Button rewardedBtn;
+
+    [FormerlySerializedAs("interads")] [SerializeField] private AdUnitVariable interstitialads;
+    [SerializeField] private AdUnitVariable rewardedads;
+    [SerializeField] private TextMeshProUGUI result;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rewardedBtn.onClick.AddListener(OnRewardBtnClick);
+        interstitialBtn.onClick.AddListener(OnInterstitialBtnClick);
+        rewardedads.OnLoaded(OnRewardAdLoaded);
+        interstitialads.OnLoaded(InterstitialAdLoaded);
+    }
+
+    private void OnRewardAdLoaded()
+    {
+        rewardedBtn.interactable = true;
+    }
+    
+    private void InterstitialAdLoaded()
+    {
+        interstitialBtn.interactable = true;
+    }
+
+    private void OnInterstitialBtnClick()
+    {
+        interstitialads.Show()
+            .OnCompleted(() =>
+            {
+                result.text = "Interstitial added";
+            })
+            .OnClosed(() =>
+            {
+                interstitialBtn.interactable = false;
+            });
+    }
+
+    private void OnRewardBtnClick()
+    {
+        rewardedads.Show()
+            .OnCompleted(() =>
+            {
+                result.text = "Reward added";
+            })
+            .OnClosed(() =>
+            {
+                rewardedBtn.interactable = false;
+            });
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
